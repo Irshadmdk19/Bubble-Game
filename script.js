d86f7ef6b320e8a5a5d6262ccfb09039f21c832c
irshadmdk19 <muirshad.ai20@sahyadri.edu.in>
2023-10-07 11:01:53 +0530

    worked on js and its completed

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ for(var i=1;i<=155;i++){
 document.querySelector('.pbot').innerHTML=bubble
 }
 
-var timer=3
+var timer=5
 function runTimer(){
    var timeint= setInterval(function(){
         if(timer>0){
@@ -22,17 +22,40 @@ function runTimer(){
         }
         else{
             clearInterval(timeint)
+            document.querySelector('.pbot').innerHTML=`<h2>Time is out</h2>`
         }
 
     },1000)
 }
 
+var ran=0
 function getNewHit(){
-    var ran= Math.floor(Math.random()*10)
+     ran= Math.floor(Math.random()*10)
     document.querySelector('#hitVal').textContent=ran
 }
+var score=0
+function increaseScore(){
+    score+=10
+    document.querySelector("#score").textContent=score
 
+}
+
+
+
+// increaseScore()
 
 makeBubble()
 runTimer()
 getNewHit()
+
+
+document.querySelector('.pbot').addEventListener(
+    "click",function(details){
+        var clickedNum=Number(details.target.textContent)
+        if(ran==clickedNum){
+            increaseScore()
+            makeBubble()
+            getNewHit()
+        }
+    }
+)
